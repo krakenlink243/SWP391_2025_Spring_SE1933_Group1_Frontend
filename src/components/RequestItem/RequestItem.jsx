f6206d1e47e5118fc3f33926498ede84d703d491
krakenlink243 <vquochoang7@gmail.com>
2025-06-08 23:26:04 +0700

    approve FE create

--- src/components/RequestItem/RequestItem.jsx
+++ src/components/RequestItem/RequestItem.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import { useState } from 'react';
 import './RequestItem.css'
-function RequestItem({requestName,onApprove,onDecline,onCheckChange}) {
+function RequestItem({requestName,onApprove,onDecline,onCheckChange,requestId,isTicked}) {
     const [isChecked,setIsChecked] = useState(false);
     const handleCheckChange = () => {
-        setIsChecked(!isChecked);
-        onCheckChange();
-    }
+      setIsChecked(!isChecked);
+      onCheckChange(requestId);
+  };
   return (
     <div className='request-item'>
       <img
-        src={isChecked ? "/icons/Approve.png" : "/icons/Checkbox.png"}
-        alt="Checkbox" onClick={handleCheckChange}
+                src={isTicked ? "/icons/Approve.png" : "/icons/Checkbox.png"}
+                alt="Checkbox"
+                onClick={() => onCheckChange(requestId)} // ✅ Dynamically update check state from parent
       />
-
-
       <div className='request-name'>{requestName}</div>
-      <img src="/icons/Decline.png" alt="" />
-      <img src="/icons/Approve.png" alt="" />
+      <img src="/icons/Decline.png" alt="" onClick={onDecline} />
+      <img src="/icons/Approve.png" alt="" onClick={onApprove}/>
     </div>
   )
 }
