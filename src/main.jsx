eead48bd72b9a11dcd783fd99376c8912a9f02bf
Phan Son <minikie456@gmail.com>
2025-06-09 08:28:04 +0700

    WIP: temp saving wait for hot fix transaction & cart page

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Cart from './Cart.jsx';
-import Transaction from './Transaction.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/transactions" element={<Transaction />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
 
