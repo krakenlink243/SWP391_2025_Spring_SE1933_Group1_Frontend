ad32cb8d9e616b782d01b76e76dfa60258c8d1a3
Phan Son <minikie456@gmail.com>
2025-06-16 23:00:53 +0700

    Fix: hotfix navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { FiSearch } from "react-icons/fi"; // Import icon search từ react-icons
 import "./Navbar.css"; // Hoặc Navbar.module.css nếu dùng CSS Modules
 import SearchBar from "../SearchBar/SearchBar"; // Import component SearchBar
+import { Navigate, useNavigate } from 'react-router-dom';
+
 /**
  * Origin @author: TS Huy
  * Refactor and re-design @author: Phan NT Son
  * @returns 
  */
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="container-fluid store-header" role="navigation">
       <div className="row">
@@ -17,7 +21,7 @@ const Navbar = () => {
             <div className="store-nav-area">
               <div className="store-nav-bg">
                 <div className="store-nav">
-                  <div className="tab"><span className="pulldown"><a href="/game">Your Store</a></span></div>
+                  <div className="tab" onClick={() => navigate("/game")}><span className="pulldown"><a>Your Store</a></span></div>
                   <div className="tab"><span className="pulldown"><a>New & Noteworthy</a></span></div>
                   <div className="tab"><span className="pulldown"><a>Categories</a></span></div>
                   <div className="tab"><span className="pulldown"><a>Points Shop</a></span></div>
