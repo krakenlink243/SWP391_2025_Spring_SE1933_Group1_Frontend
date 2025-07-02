1c7931c839f07a961501a65be2898824ef386a88
krakenlink243 <vquochoang7@gmail.com>
2025-07-02 15:49:24 +0700

    fix send game to admin

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ import EmailSettings from "./components/EmailChange/EmailSettings";
 import { jwtDecode } from "jwt-decode";
 import { isTokenExpired } from "./utils/validators";
 import { CartCountProvider } from "./utils/TotalInCartContext";
-import SendGameToAdmin from "./pages/SendGametoAdmin";
+import SendGametoAdmin from "./pages/SendGametoAdmin";
 import AIGeneratorFrontend from "./pages/test"; // TEST
 
 function AppRoutes() {
